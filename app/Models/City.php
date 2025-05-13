7a5fe6fac2ddaadb48ca1fffc7a65f8db2d70c28
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-05-13 11:09:10 +0200

    added fillable to city

--- app/Models/City.php
+++ app/Models/City.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'country',
+        'continent',
+        'population',
+        'latitude',
+        'longitude',
+        'known_for',
+        'founded_year',
+        'is_capital',
+        'annual_tourists'
+    ];
 }
